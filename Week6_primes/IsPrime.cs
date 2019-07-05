6d38d0220317be04dbe3d611c92271ee0799d61b
Rachel Hague <r.m.hague@gmail.com>
2019-07-05 11:06:40 -0400

    Add project files.

---
+++ Week6_primes/IsPrime.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6_primes
+{
+    class TestPrime
+    {
+        public static bool IsPrime(int intPrimeTest)
+        {
+            if (intPrimeTest <= 1)
+                return false;
+
+            else if (intPrimeTest < 4)
+            {
+                return true;
+            }
+
+            else if (intPrimeTest % 2 == 0)
+            {
+                return false;
+            }
+
+            else
+            {
+                int k = 3;
+                int j = 1;
+
+                do
+                {
+                    j = intPrimeTest % k;
+                    k += 2;
+                }
+                while ((k <= Math.Sqrt(intPrimeTest)) && (j != 0));
+
+                if (j == 0)
+                {
+                    return false;
+                }
+
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public static List<int> PrimesUpTo(int upTo)
+        {
+            bool primeBool;
+            var primeList = new List<int>();
+
+            for (int i = 0; i <= upTo; i++)
+            {
+                primeBool = IsPrime(i);
+                if (primeBool == true)
+                {
+                    primeList.Add(i);
+                }
+            }
+            return primeList;
+        }
+    }
+}
---
+++ Week6_primes/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week6_primes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void btn_checkPrime_click(object sender, RoutedEventArgs e)
+        {
+            bool primeBool;
+            int x;
+            if (int.TryParse(textbox_toCheck.Text, out x))
+            {
+                primeBool = TestPrime.IsPrime(x);
+
+                if (x > 100)
+                {
+                    lbl_primeResult.Content = "Please enter an integer under 100.";
+                }
+
+               else if (primeBool == true)
+                {
+                    lbl_primeResult.Content = x + " is a prime number.";
+                }
+
+                else
+                {
+                    lbl_primeResult.Content = x + " is not a prime number.";
+                }
+            }
+
+            else {
+                lbl_primeResult.Content = "Please enter an integer under 100.";
+            }
+        }
+
+        public void btn_listPrimes_click(object sender, RoutedEventArgs e)
+        {
+            var primeList = new List<int>();
+            primeList = TestPrime.PrimesUpTo(100);
+            ListBox.ItemsSource = primeList;
+        }
+
+
+    }
+}
